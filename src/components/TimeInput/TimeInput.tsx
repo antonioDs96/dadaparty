8322febc5d9f8bcf46d841564d8fe308e7aec574
desantiswebscience <antonio.desantis@webscience.it>
2023-08-21 16:55:59 +0200

    fix: clock icon

--- src/components/TimeInput/TimeInput.tsx
+++ src/components/TimeInput/TimeInput.tsx
@@ -1,7 +1,8 @@
+'use client';
+
 import React, {FC} from 'react';
 import Image from "next/image";
 import ClockIconDark from "../../../public/clock.svg";
-import ClockIconWhite from "../../../public/clock-dark.svg";
 
 interface TimeInputProps {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -12,8 +13,29 @@ interface TimeInputProps {
 
 const TimeInput: FC<TimeInputProps> = ({value, onChange, name, isDarkMode}) => {
     return (
-        <div style={{position: 'relative', display: 'inline-block', width: "100%"}}>
-            <input
+        isDarkMode ?
+            <div style={{position: 'relative', display: 'inline-block', width: "100%"}}>
+                <input
+                    id={name}
+                    className="dark:bg-slate-800 dark:text-white/80 border border-gray-300 text-sm rounded-lg focus:border-fuchsia-500 focus:ring-fuchsia-500/20 block w-full p-2.5"
+                    placeholder="17-51"
+                    type="time"
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                />
+                <span
+                    style={{
+                        position: 'absolute',
+                        right: 10,
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        pointerEvents: 'none'
+                    }}>
+               <Image src={ClockIconDark} alt={'CLOCK'} width={20} height={20}/>
+      </span>
+            </div>
+            : <input
                 id={name}
                 className="dark:bg-slate-800 dark:text-white/80 border border-gray-300 text-sm rounded-lg focus:border-fuchsia-500 focus:ring-fuchsia-500/20 block w-full p-2.5"
                 placeholder="17-51"
@@ -22,19 +44,6 @@ const TimeInput: FC<TimeInputProps> = ({value, onChange, name, isDarkMode}) => {
                 value={value}
                 onChange={onChange}
             />
-            <span
-                style={{
-                    position: 'absolute',
-                    right: 10,
-                    top: '50%',
-                    transform: 'translateY(-50%)',
-                    pointerEvents: 'none'
-                }}>
-                {isDarkMode ? <Image src={ClockIconDark} alt={'CLOCK'} width={20} height={20}/> :
-                    <Image src={ClockIconWhite} alt={'CLOCK'} width={20} height={20}/>
-                }
-      </span>
-        </div>
     );
 }
 
